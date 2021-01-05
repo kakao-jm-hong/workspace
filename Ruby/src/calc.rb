247a9e0f22a4031597ff05cd3941e2591d3f50c1
redbin <wjdals0471@naver.com>
2021-01-05 10:59:26 +0900

    first init

---
+++ Ruby/src/calc.rb
@@ -0,0 +1,18 @@
+# puts : 화면에 출력
+puts '화면에 출력'
+puts 1+2
+
+# 정수
+puts '정수'
+puts 5
+
+# 실수
+puts '실수'
+puts 54.321
+
+# 간단한 사칙 연산
+puts '간단한 사칙 연산'
+puts 1.0 + 2.0
+puts 2.0 * 3.0
+puts 9/2
+puts 9.0/2
