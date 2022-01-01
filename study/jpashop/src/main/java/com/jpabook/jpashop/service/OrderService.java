84ce6703e59194f9733389b87906bf24352eb428
Hongjeongmin <wjdals0471@naver.com>
2022-01-01 16:41:30 +0900

    jpa 학습 도메인 모델 패턴 , 서비스 스크립트 패턴 이런게 있음

---
+++ study/jpashop/src/main/java/com/jpabook/jpashop/service/OrderService.java
@@ -0,0 +1,67 @@
+package com.jpabook.jpashop.service;
+
+import com.jpabook.jpashop.domain.Delivery;
+import com.jpabook.jpashop.domain.Member;
+import com.jpabook.jpashop.domain.Order;
+import com.jpabook.jpashop.domain.OrderItem;
+import com.jpabook.jpashop.domain.item.Item;
+import com.jpabook.jpashop.repository.ItemRepository;
+import com.jpabook.jpashop.repository.MemberRepository;
+import com.jpabook.jpashop.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class OrderService {
+
+    private final OrderRepository orderRepository;
+    private final MemberRepository memberRepository;
+    private final ItemRepository itemRepository;
+
+    //주문
+    @Transactional
+    public Long order(Long memberId, Long itemId, int count) {
+        // 엔티티 조회
+        Member member = memberRepository.findOne(memberId);
+        Item item = itemRepository.findOne(itemId);
+
+        //배송정보 조회
+        Delivery delivery = new Delivery();
+        delivery.setAddress(member.getAddress());
+
+        //주문 상품 생성
+        OrderItem orderItem = OrderItem.createOrderItem(item, item.getPrice(), count);
+
+        //주문 생성
+        Order order = Order.createOrder(member, delivery, orderItem);
+
+        // 주문 저장
+        orderRepository.save(order);
+
+        return order.getId();
+
+        // 엔티티가 비즈니스로직을 가지고 있는것을 도메인 모델 패턴이라고 한다.
+    }
+
+    //취소
+
+    @Transactional
+    public void cancelOrder(Long orderId) {
+        // 주문엔티티 조회
+        Order order = orderRepository.findOne(orderId);
+        // 주문취소
+        order.cancel();
+
+    }
+    //검색
+
+//    public List<Order> findOrders(OrderSearch orderSearch) {
+//        return orderRepository.findAll(orderSearch);
+//    }
+
+}
